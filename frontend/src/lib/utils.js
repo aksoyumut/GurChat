214eb9dfa7994842e45642cadc3658b8b4630907
Umut AKSOY <152719549+aksoyumut@users.noreply.github.com>
2025-05-19 21:17:12 +0300

    1.6

--- frontend/src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatMessageTime(date) {
-  return new Date(date).toLocaleTimeString("tr-TR", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-    timeZone: "Europe/Istanbul", // İstanbul saatine göre göster
-  });
-}
--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "Light",
-  setTheme: (theme) => {
-    localStorage.setItem("chat-theme", theme);
-    set({ theme });
-  },
-}));
